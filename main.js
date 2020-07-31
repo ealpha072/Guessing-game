0402419b17a9982336033f579040f604dfa801cf
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2020-07-31 15:51:20 +0300

    Added initial files

---
+++ main.js
@@ -0,0 +1,86 @@
+//constants
+let input  = document.getElementById('input');
+let para = document.querySelector('.guess-holder');
+let para2 = document.querySelector('.remaining-guess');
+let head = document.querySelector('.game-status');
+let submit = document.getElementById('submit')
+let gameDiv = document.getElementById('game-holder')
+const restart = document.createElement('button');
+let guesses = [];
+let numOfGuesses = 1;
+//let guessDisplay = document.querySelector('p #game-holder');
+let random = Math.floor(Math.random()*100)+1;
+
+console.log(random)
+function endGame(){
+    input.Value = '';
+    input.setAttribute('disabled','');
+    restart.textContent= 'Start new Game';
+    gameDiv.appendChild(restart);
+    restart.addEventListener('click',(e)=>{
+        e.preventDefault();
+        newGame();
+    })
+}
+
+function newGame(){
+    input.removeAttribute('disabled');
+    random = Math.floor(Math.random()*100)+1;
+    numOfGuesses = 1;
+    guesses = [];
+    para.innerHTML = '';
+    para2.innerHTML = '';
+    gameDiv.removeChild(restart);
+}
+
+function checkGuess(guess){
+    if(guess === random){
+        head.innerHTML ='Guessed value is correct';
+        endGame()
+    }else if(guess > random){
+        head.innerHTML = '';
+        head.innerHTML = 'Too high, try a lower value!'
+    }else if(guess < random){
+        head.innerHTML = '';
+        head.innerHTML = 'Too low, try a higher value!'
+    }
+}
+
+function validate(guess){
+    if(guess > 100){
+        alert('Enter a number below 100!!');
+        input.value = '';
+    }else if(isNaN(guess)){
+        alert('You must enter a number!!')
+    }
+    else if(guess < 1){
+        alert('Enter a number above 0!!')
+        input.value = '';
+    }else{
+        guesses.push(guess);
+        if(numOfGuesses === 10){
+            displayGuess(guess);
+            head.innerHTML = '';
+            head.innerHTML = `Game over, the number is ${random} !!`
+            endGame();
+        }else{
+            checkGuess(guess)
+            displayGuess(guess);
+            
+        }
+    }
+}
+
+
+function displayGuess(guess){
+    input.value = '';
+    para.innerHTML += guess+' '
+    numOfGuesses++;
+    para2.innerHTML = `${11- numOfGuesses} `;
+}
+
+submit.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const guess = parseInt(input.value);
+    validate(guess)
+})
